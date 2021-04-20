0272a2e4ddc987692f25c498844fb913e4833fec
joeribouwman25 <joeri-bouwman@live.nl>
2021-04-20 23:02:16 +0200

    added routes and 404 error message

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('welcome')
 })
+
+app.get('/signUp', (req, res) => {
+    res.send('time to create a profile')
+  })
+
+
+app.get('/myprofile', (req, res) => {
+    res.send('my profile')
+})
+
+app.use(function (req, res, next) {
+    res.status(404).send("Page not found")
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
