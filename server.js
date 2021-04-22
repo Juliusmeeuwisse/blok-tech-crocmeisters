2aea794c629a7d8087d9c9f836cb2c65783f6b85
joeribouwman25 <joeri-bouwman@live.nl>
2021-04-22 14:44:55 +0200

    changed map structure

--- server.js
+++ server.js
@@ -2,21 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+
+
 app.get('/', (req, res) => {
-  res.send('welcome')
+  res.send('<h1> Welcome {name} <h1/>')
+  console.log(template({ name: "joeri" }));
 })
 
-app.get('/signUp', (req, res) => {
-    res.send('time to create a profile')
+app.get('/settings', (req, res) => {
+    res.send('settings')
   })
 
 
 app.get('/myprofile', (req, res) => {
     res.send('my profile')
+
 })
 
+
 app.use(function (req, res, next) {
-    res.status(404).send("Page not found")
+    res.status(404).send("404 Page not found")
   })
 
 
