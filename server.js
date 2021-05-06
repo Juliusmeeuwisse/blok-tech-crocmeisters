ed2244929976d47abb121ebcf418375310de43af
joeribouwman25 <joeri-bouwman@live.nl>
2021-05-07 00:20:56 +0200

    📈 added custom id's

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request');
 const handlebars = require('express-handlebars');
+const { v4: uuidv4 } = require('uuid');
 require('dotenv').config()
 
 const app = express()
@@ -11,7 +12,6 @@ const app = express()
 const port = process.env.PORT
 const url = process.env.DB_URL;
 let users = null;
-let idCheck;
 app.set('view engine', 'hbs');;
 app.set('views', 'views');;
 app.engine('hbs', handlebars({extname: 'hbs'}));
@@ -36,25 +36,23 @@ MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
     db = client.db(process.env.DB_NAME).collection('user')
   }
 })
-    
-//page templates      
-let match = []
+// variables
+let myID = "1128bae9-5a62-4905-a404-2c9386e26df9"
 let heartIconGreen = "/images/icons/green heart.png"
 let heartIcon = "/images/icons/white heart.png"
 
+//page templates      
 app.get('/', (req, res) => {
-
-
-
-  //find user from database
-  users.findOne((err, user) =>{
+  //find random user from database
+  users.aggregate([{$sample: {
+    size: 1}}]).toArray( (err, result) => {
     if (err) throw err; 
     else{
       //render the user
       let banner = "/images/banners/Banner MMM-home.png"
-      let userProfile = user
+      let userProfile = result[0]
       console.log()
-        res.render('home',{
+        res.render('home', {
           heartIcon: heartIcon,
           banner: banner,
           userProfile: userProfile,
@@ -64,33 +62,54 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  users.findOne((err, user) => {
-    if (err) { console.log(err) }
-    else{      
-        if(req.body.like == 'true'){
-          users.updateOne({name: "john"},
-            {$set: {likes: true}},(err, like) => {
-            if(err){ console.log(err) }
-            else{
-              // console.log(like)
-            }
+  //returns one random user
+  let randomUser = users.aggregate([{$sample: {
+        size: 1}}]).toArray( (err, result) => {
+
+    let userID = result[0].id;
+    if(err) throw(err);
+    else{
+      let userProfile = result[0]
+      console.log(userProfile.seen)
+      let banner = "/images/banners/Banner MMM-home.png"
+      if(req.body.like == 'true'){
+      users.findOneAndUpdate({'id':myID}, {$push: {likes: userProfile.id, seen: userProfile.id}}, 
+      (err, user) => {
+        if (err){console.log(err)}
+        else if(userProfile.seen.includes(userID)){
+          randomUser
+        }
+        else if(result[0].likes.includes(myID)){
+          console.log('match🥳')
+          users.findOneAndUpdate({'id':myID}, {$push: {matches: userProfile.id}},
+          (err, res) =>{
+            if(err){console.log(err)}
           })
+        }
+      })
       }
+      else if(req.body.like == 'false'){
+        users.findOneAndUpdate({'id':myID}, {$push: {seen: userProfile.id}},
+        (err, res) => {
+          if(err){
+            console.log(err)
+          }         
+          else if(userProfile.seen.includes(userID)){
+            randomUser
+          }
+          else{
+            console.log('no match')
+          }
+        })
+      }
+      res.render('home',{
+        banner: banner,
+        heartIcon: heartIcon,
+        userProfile: userProfile,
+      })
     }
-  }
+  })
 })
-
-// function update(req, res) {
-//   if(req.body.like == 'true'){
-//     users.updateone(
-//       {name: "john"},
-//       {$set: {likes: 'true'}})
-//       res.redirect('/')
-//     }
-// }
-  
-
- 
 
 
 
@@ -132,51 +151,3 @@ app.use(function (req, res, next) {
 
 
 
-   // app.post('/', (req, res) => {
-  //   //returns one random user
-  //   let randomUser = users.aggregate([{$sample: {
-  //         size: 1}}]).toArray( (err, result) => {
-
-  //     let userID = result[0]._id.toString();
-  //     if(err) {
-  //       console.log(err)
-  //     }
-  //     else{
-  //       let userProfile = result[0]
-  //       let banner = "/images/banners/Banner MMM-home.png"
-  //       if(req.body.like == 'true'){
-  //       users.findOneAndUpdate({"name":"joeri"}, {$push: {likes: userProfile._id, seen: userProfile._id}}, 
-  //       (err, user) => {
-  //         if (err){
-  //           console.log(err)
-  //         }
-  //         else if(userID === idCheck[0]){
-  //           randomUser
-  //         }
-  //         else if(user.likes === userID){
-  //           console.log(user + 'update succesfull🥳')
-  //         }
-  //       })
-  //       }
-  //       else if(req.body.like == 'false'){
-  //         users.findOneAndUpdate({"name":"joeri"}, {$push: {seen: userProfile._id}},
-  //         (err, res) => {
-  //           if(err){
-  //             console.log(err)
-  //           }         
-  //           else if(userID === idCheck[0]){
-  //             randomUser
-  //           }
-  //           else{
-  //             console.log('no match')
-  //           }
-  //         })
-  //       }
-  //       res.render('home',{
-  //         banner: banner,
-  //         heartIcon: heartIcon,
-  //         userProfile: userProfile,
-  //       })
-  //     }
-  //   })
-  // })
