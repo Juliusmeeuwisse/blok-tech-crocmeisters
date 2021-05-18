17861f363eedcf10fb980f45476874dc75f36b89
joeribouwman25 <joeri-bouwman@live.nl>
2021-05-18 21:27:43 +0200

    👷🏻‍♂️ lclosure would only work in chrome

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,24 @@
-
+// play music and change play button
 const music = document.querySelectorAll('audio')
 const button = document.getElementsByClassName('play')
 const icon = document.querySelectorAll('span')
+let a = true
 
 for (let i = 0; i < button.length; i++) {
-  const x = playMusic()
-  button[i].addEventListener('click', x)
-
-  function playMusic () {
-    let a = true
-    function closure () {
-      if (a === true) {
-        music[i].play()
-        icon[i].classList.add('pause')
-        a = false
-      } else {
-        music[i].pause()
-        a = true
-        icon[i].classList.remove('pause')
-      }
+  const playMusic = () => {
+    if (a === true) {
+      music[i].play()
+      icon[i].classList.add('pause')
+      a = false
+    } else {
+      music[i].pause()
+      a = true
+      icon[i].classList.remove('pause')
     }
-    return closure
   }
+  button[i].addEventListener('click', playMusic)
 }
-
+// matching
 const myID = document.getElementsByClassName('matchID')[1].innerHTML
 const matchArray = document.getElementsByClassName('matchID')[0].innerHTML.split(',')
 const match = matchArray.find((id) => id.includes(myID))
